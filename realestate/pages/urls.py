d50314efd2ddf1bef1fdec7f86cc15ff216c02e8
Yesil <yesil@gmail.com>
2025-02-10 23:22:45 +0300

    html css edit

--- realestate/pages/urls.py
+++ realestate/pages/urls.py
@@ -3,5 +3,6 @@ from .views import *
 urlpatterns = [
     path('',index,name='index'),
     path('advertisements',advertisements,name='advertisements'),
-    path('contact',contact,name='contact')
+    path('contact',contact,name='contact'),
+    path('<int:id>/<slug:slug>/',house_detail,name='detail')
 ]
--- realestate/pages/views.py
+++ realestate/pages/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from houses.models import Property
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    is_featured = Property.objects.filter(available=True, is_featured=True)
+    context={
+        'is_featured':is_featured
+    }
+
+    return render(request,'index.html',context)
 
 def advertisements(request):
     propertys = Property.objects.filter(available=True)
     context = {
-        'propertys': propertys
+        'propertys': propertys,
+
     }
     return render(request,'properties.html',context)
-
+def house_detail(request,id,slug):
+    house=get_object_or_404(Property,pk=id,slug=slug)
+    context={
+        'house':house
+    }
+    return render(request,'property-details.html',context)
 def contact(request):
     return render(request,'contact.html')
